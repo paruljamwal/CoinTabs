89f8a95850d82c1d369e22bcfcee4111ffe656f1
paruljamwal <paruljamwal2000@gmail.com>
2022-12-17 13:50:26 +0530

    delete routing

--- CoinTab_Backend/src/Controller/user.controller.js
+++ CoinTab_Backend/src/Controller/user.controller.js
@@ -49,6 +49,18 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+
+// delete User...
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id).lean().exec();
+    return res.status(201).send({ user });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+
 // delete User...
 router.delete("/delete", async (req, res) => {
   try {
@@ -56,6 +68,18 @@ router.delete("/delete", async (req, res) => {
     return res.status(201).send({ user });
   } catch (error) {
     return res.status(500).send({ message: error.message });
+  }
+});
+
+
+ //patch User...
+ router.patch("/:id", async(req,res)=>{
+  try {
+      const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec();
+      return res.status(201).send({user});
+
+  } catch (error) {
+      return res.status(500).send({ message: error.message }); 
   }
 });
 
